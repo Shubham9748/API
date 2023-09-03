12f218ccf958a521122638dd814a6a9049c7058e
Shubham Bhagat <bhagatsubham00@gmail.com>
2023-09-03 13:23:23 +0530

    cors

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const app=express();
+const cors =require('cors');
 
+
+app.use(cors());
+//app.use(cors('http://localhost:3000"));
 const connectDB =require('./db/connect');
 
 const product_routes =require('./routes/product');
