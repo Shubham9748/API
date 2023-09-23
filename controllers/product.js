08d9e6abedbdfadede58b203195f29da93c11564
Shubham Bhagat <bhagatsubham00@gmail.com>
2023-09-23 12:28:51 +0530

    cors

--- controllers/product.js
+++ controllers/product.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 
 const getAllProducts =async (req, res)=>{
     const myData= await Product.find({});
-    res.status(200).json({myData});
+    res.status(200).json(myData);
 };
 
 module.exports = {getAllProducts};
